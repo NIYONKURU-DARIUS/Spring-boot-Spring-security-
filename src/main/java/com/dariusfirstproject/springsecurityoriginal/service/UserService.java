042168b07c4d22133b070d6c346ce46746f94b1b
NIYONKURU Darius <dariusniyonkuru351@gmail.com>
2026-01-11 21:02:46 +0200

    Basic JWT

---
+++ src/main/java/com/dariusfirstproject/springsecurityoriginal/service/UserService.java
@@ -0,0 +1,34 @@
+package com.dariusfirstproject.springsecurityoriginal.service;
+
+import com.dariusfirstproject.springsecurityoriginal.model.Users;
+import com.dariusfirstproject.springsecurityoriginal.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private JWTService jwtService;
+    @Autowired
+    private UserRepo repo;
+    @Autowired
+    AuthenticationManager authManager;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+    public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
+       return repo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        if(authentication.isAuthenticated()){
+
+            return jwtService.generateToken(user.getUsername());
+        } else return "fail";
+    }
+}
